442c96b49e35dafd37897ce43750995dd22caf74
Chen <32901369+Chen@users.noreply.github.com>
2021-05-04 13:02:38 +0100

    Built `Fix the title of the website in seo file; refine the publications page and publication page`

--- zh/markdown_generator/publications.py
+++ zh/markdown_generator/publications.py
@@ -76,8 +76,8 @@ for row, item in publications.iterrows():
     
     md += """\npermalink: /publication/""" + html_filename
     
-    if len(str(item.excerpt)) > 5:
-        md += "\nexcerpt: '" + html_escape(item.excerpt) + "'"
+    #if len(str(item.excerpt)) > 5:
+    #    md += "\nexcerpt: '" + html_escape(item.excerpt) + "'"
     
     md += "\ndate: " + str(item.pub_date) 
     
@@ -97,10 +97,10 @@ for row, item in publications.iterrows():
     #if len(str(item.paper_url)) > 5:
     #    md += "\n\n<a href='" + item.paper_url + "'>Download paper here</a>\n" 
         
-    #if len(str(item.excerpt)) > 5:
-    #    md += "\n" + html_escape(item.excerpt) + "\n"
+    if len(str(item.excerpt)) > 5:
+        md += "\nAbstract \n------ \n" + html_escape(item.excerpt) + "\n"
         
-    # md += "\nRecommended citation: " + item.citation
+    md += "\nRecommended citation: \n------ \n" + item.citation
     
     md_filename = os.path.basename(md_filename)
        
